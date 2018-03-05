055e4cd7dd17649d7182bbc2cc8e9e3069a08877
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-05 13:57:58 -0300

    Improves the size of the axes

--- src/chartsLib.js
+++ src/chartsLib.js
@@ -1,28 +1,34 @@
 import * as d3 from "d3";
 
 export const renderQueuChart = () => {
+  const margins = { top: 20, right: 20, bottom: 20, left: 20 };
+  const width = 600;
+  const height = 400;
+  const chartWidth = width - margins.left - margins.right;
+  const chartHeight = height - margins.top - margins.bottom;
+
   const yScale = d3
     .scaleLinear()
-    .domain([30, 0])
-    .range([0, 280]);
+    .domain([0, 30])
+    .range([chartHeight, 0]);
   const yAxis = d3.axisLeft(yScale);
 
   const xScale = d3
     .scaleLinear()
     .domain([0, 10])
-    .range([0, 280]);
+    .range([0, chartWidth]);
 
   const xAxis = d3.axisBottom(xScale);
 
   d3
     .select("#svg")
     .append("svg")
-    .attr("width", "60%")
-    .attr("height", 400)
+    .attr("width", width)
+    .attr("height", height)
     .append("g")
-    .attr("transform", "translate(30,90)")
+    .attr("transform", `translate(30, ${margins.top})`)
     .call(yAxis)
     .append("g")
-    .attr("transform", "translate(0,280)")
+    .attr("transform", `translate(0, ${chartHeight})`)
     .call(xAxis);
 };
