2de16ce2946a99f815df94186ba15268a5f45282
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-14 17:14:08 +0000

    Adds a grid layout

--- src/Commits.js
+++ src/Commits.js
@@ -11,26 +11,39 @@ class Commits extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Commits</h1>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          {this.state.developers.map((dev, i) => (
-            <li
-              key={i}
-              style={{
-                borderBottom: "1px solid #ccc",
-                padding: "5px",
-                marginBottom: "5px"
-              }}
-            >
-              <div>
-                <a href="">{dev.name}</a>
-              </div>
-              <div style={{ fontSize: "0.8rem", color: "#999" }}>
-                {dev.email}
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className="row">
+          <div className="col-12 col-md-4 offset-md-4">
+            <h1 style={{ textAlign: "center" }}>Commits</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-4 offset-md-4">
+            <input type="text" placeholder="Email" className="form-control" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-4 offset-md-4">
+            <ul style={{ listStyle: "none", padding: 0 }}>
+              {this.state.developers.map((dev, i) => (
+                <li
+                  key={i}
+                  style={{
+                    borderBottom: "1px solid #ccc",
+                    padding: "5px",
+                    marginBottom: "5px"
+                  }}
+                >
+                  <div>
+                    <a href="">{dev.name}</a>
+                  </div>
+                  <div style={{ fontSize: "0.8rem", color: "#999" }}>
+                    {dev.email}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
