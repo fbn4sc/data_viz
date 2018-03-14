5c076b83a89e8aec1d6bbed29793ce5aae5bd034
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-14 17:03:09 +0000

    Adds page to see list of developers

--- src/Commits.js
+++ src/Commits.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import apiClient from "./apiClient";
 
-const Commits = () => {
-  return (
-    <div className="container">
-      <h1>Commits</h1>
-    </div>
-  );
-};
+class Commits extends Component {
+  state = { developers: [] };
+
+  componentDidMount() {
+    apiClient.getDevelopers().then(developers => this.setState({ developers }));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Commits</h1>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          {this.state.developers.map((dev, i) => (
+            <li
+              key={i}
+              style={{
+                borderBottom: "1px solid #ccc",
+                padding: "5px",
+                marginBottom: "5px"
+              }}
+            >
+              <div>
+                <a href="">{dev.name}</a>
+              </div>
+              <div style={{ fontSize: "0.8rem", color: "#999" }}>
+                {dev.email}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Commits;
---
+++ src/apiClient.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: "http://delta-api.herokuapp.com"
+});
+
+const apiClient = {
+  getDevelopers: () => {
+    return axiosInstance
+      .get(`/developers`)
+      .then(res => res.data)
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+
+export default apiClient;
